83fd96f371e62c4ef3a288cbbad9eb24810bc2cf
PeterBish <alberto.pas91@gmail.com>
2018-12-17 16:23:57 +0100

    Delete BookRepository.java

--- src/main/java/it/biblos/interfaces/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package it.biblos.interfaces;
-
-import it.biblos.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Integer, Book> {
-
-}
